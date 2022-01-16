c55a5a5dcf55c2e14237d6153391a1053581212a
Hilario Villamar <hilario_villamar@outlook.com>
2022-01-15 19:45:56 -0500

    Commit del proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+#CYcleGAN
+
+D:\data\patient\
+
+![img.png](notebook_images/img.png)
---
+++ config.py
@@ -0,0 +1,13 @@
+import configparser
+import os
+
+
+class Params:
+    CFG = configparser.ConfigParser()
+    CFG.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.cfg'), encoding='utf-8')
+
+    PRM = CFG.get('DEFAULT', 'env')
+    path_abs = CFG[PRM]['path_abs']
+    path_dcm = os.path.join(path_abs, 'DCM')
+    path_jpg = os.path.join(path_abs, 'JPG')
+    path_samples = os.path.join(path_abs, 'samples_cyclegan')
